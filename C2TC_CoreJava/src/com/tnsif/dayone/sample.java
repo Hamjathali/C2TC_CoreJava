b6be8a3b752347168d631954eea3ed8ec1df9c9a
hamjath <syedanwar@gmail.com>
2025-10-07 14:02:48 +0530

    Package name changed

---
+++ C2TC_CoreJava/src/com/tnsif/dayone/sample.java
@@ -0,0 +1,10 @@
+package com.tnsif.dayone;
+
+public class sample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is my First Code");
+	}
+
+}
---
+++ C2TC_CoreJava/src/com/tnsif/daytwo/IfDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.daytwo;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		
+		int number = 10;
+		
+		if (number > 0) {
+            System.out.println(number + " is a positive number.");
+        }
+
+	}
+
+}
